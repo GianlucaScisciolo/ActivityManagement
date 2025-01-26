b312e4694e068cd2afe01b1d443b2ecb3abe3393
GianlucaScisciolo <gianlucascisciolo@gmail.com>
2025-01-26 12:33:44 +0100

    varie modifiche

--- philia/db/server.js
+++ philia/db/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 const app = express();
 
 app.use(cors());
